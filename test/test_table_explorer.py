3e3984f3eb64382b34287b24f29ffa7895520734
vmarquet <vincent.marquet1@free.fr>
2016-09-25 20:36:55 +0200

    add unit tests for TableExplorer.row() and TableExporer.col()

--- test/test_table_explorer.py
+++ test/test_table_explorer.py
@@ -68,7 +68,6 @@ class TestTableExplorer(unittest.TestCase):
         self.assertEqual(table.height, 1)
 
 
-
     def test_number_of_cells(self):
         table = TableExplorer(self.table_one_cell)
         self.assertEqual(len(table.all()), 1)
@@ -87,4 +86,61 @@ class TestTableExplorer(unittest.TestCase):
 
         table = TableExplorer(self.table_one_row)
         self.assertEqual(len(table.all()), 4)
+
+
+    def test_number_of_cells_in_col(self):
+        table = TableExplorer(self.table_one_cell)
+        self.assertEqual(len(table.col(0)), 1)
+
+        table = TableExplorer(self.table_simple)
+        self.assertEqual(len(table.col(0)), 3)
+        self.assertEqual(len(table.col(1)), 3)
+        self.assertEqual(len(table.col(2)), 3)
+
+        table = TableExplorer(self.table_rowspan_colspan)
+        self.assertEqual(len(table.col(0)), 4)
+        self.assertEqual(len(table.col(1)), 3)
+        self.assertEqual(len(table.col(2)), 3)
+        self.assertEqual(len(table.col(3)), 4)
+
+        table = TableExplorer(self.table_one_cell_rowspan_colspan)
+        self.assertEqual(len(table.col(0)), 1)
+
+        table = TableExplorer(self.table_one_column)
+        self.assertEqual(len(table.col(0)), 4)
+
+        table = TableExplorer(self.table_one_row)
+        self.assertEqual(len(table.col(0)), 1)
+        self.assertEqual(len(table.col(1)), 1)
+        self.assertEqual(len(table.col(2)), 1)
+        self.assertEqual(len(table.col(3)), 1)
+
+
+    def test_number_of_cells_in_row(self):
+        table = TableExplorer(self.table_one_cell)
+        self.assertEqual(len(table.row(0)), 1)
+
+        table = TableExplorer(self.table_simple)
+        self.assertEqual(len(table.row(0)), 3)
+        self.assertEqual(len(table.row(1)), 3)
+        self.assertEqual(len(table.row(2)), 3)
+
+        table = TableExplorer(self.table_rowspan_colspan)
+        self.assertEqual(len(table.row(0)), 3)
+        self.assertEqual(len(table.row(1)), 2)
+        self.assertEqual(len(table.row(2)), 3)
+        self.assertEqual(len(table.row(3)), 3)
+        self.assertEqual(len(table.row(4)), 3)
+
+        table = TableExplorer(self.table_one_cell_rowspan_colspan)
+        self.assertEqual(len(table.row(0)), 1)
+
+        table = TableExplorer(self.table_one_column)
+        self.assertEqual(len(table.row(0)), 1)
+        self.assertEqual(len(table.row(0)), 1)
+        self.assertEqual(len(table.row(0)), 1)
+        self.assertEqual(len(table.row(0)), 1)
+
+        table = TableExplorer(self.table_one_row)
+        self.assertEqual(len(table.row(0)), 4)
 
